966a1f33ace40bc27169bc54789463f1ce25dd0d
KrakeNocto <octo2kraken@gmail.com>
2024-09-19 17:56:47 +0300

    Update storage_upd_0.5.1.sh

--- storage_upd_0.5.1.sh
+++ storage_upd_0.5.1.sh
@@ -12,7 +12,7 @@ chmod +x /root/0g-storage-node/target/release/zgs_node
 mv $HOME/config-testnet-turbo.toml $HOME/0g-storage-node/run/
 
 sed -i 's|^network_boot_nodes = .*|network_boot_nodes = ["/ip4/54.219.26.22/udp/1234/p2p/16Uiu2HAmTVDGNhkHD98zDnJxQWu3i1FL1aFYeh9wiQTNu4pDCgps","/ip4/52.52.127.117/udp/1234/p2p/16Uiu2HAkzRjxK2gorngB1Xq84qDrT4hSVznYDHj6BkbaE4SGx9oS","/ip4/121.43.181.26/udp/1234/p2p/16Uiu2HAm2k6ua2mGgvZ8rTMV8GhpW71aVzkQWy7D37TTDuLCpgmX"]|g' $HOME/0g-storage-node/run/config-testnet.toml
-sed -ie 's|http://[^ ]*|http://157.90.128.250:22345|' /etc/systemd/system/zgstorage.service
+sed -ie 's|http://[^ ]*|http://136.243.93.159:8545|' /etc/systemd/system/zgstorage.service
 sed -ie 's|/root/0g-storage-node/run/[^ ]*|/root/0g-storage-node/run/config-testnet-turbo.toml|' /etc/systemd/system/zgstorage.service
 rm -rf $HOME/0g-storage-node/run/db
 
