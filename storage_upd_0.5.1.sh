d8896d2ce500b1238ad3e944acac4f8e8c88197e
KrakeNocto <octo2kraken@gmail.com>
2024-09-19 14:47:33 +0300

    Update storage_upd_0.5.1.sh

--- storage_upd_0.5.1.sh
+++ storage_upd_0.5.1.sh
@@ -21,14 +21,6 @@ echo "Storage version is $(/root/0g-storage-node/target/release/zgs_node --versi
 
 sleep 2
 
-while true; do 
-    response=$(curl -s -X POST http://localhost:5678 -H "Content-Type: application/json" -d '{"jsonrpc":"2.0","method":"zgs_getStatus","params":[],"id":1}')
-    logSyncHeight=$(echo $response | jq '.result.logSyncHeight')
-    connectedPeers=$(echo $response | jq '.result.connectedPeers')
-    echo -e "logSyncHeight: \033[32m$logSyncHeight\033[0m, connectedPeers: \033[34m$connectedPeers\033[0m"
-    sleep 5; 
-done
-
 tail -f $HOME/0g-storage-node/run/log/zgs.log.$(TZ=UTC date +%Y-%m-%d)
 
 rm storage_upd_0.5.1.sh
