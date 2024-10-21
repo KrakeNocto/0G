5c3e75ca754e19501ef760fae8477e744e73b5d3
KrakeNocto <octo2kraken@gmail.com>
2024-10-21 18:37:17 +0300

    Create daclient_upd.sh

---
+++ daclient_upd.sh
@@ -0,0 +1,10 @@
+systemctl stop daclient
+
+wget http://116.202.52.168:12312/batcher http://116.202.52.168:12312/combined http://116.202.52.168:12312/server
+
+mv batcher /root/0g-da-client/disperser/bin/ && mv combined /root/0g-da-client/disperser/bin/ && mv server /root/0g-da-client/disperser/bin/
+chmod +x /root/0g-da-client/disperser/bin/batcher && chmod +x /root/0g-da-client/disperser/bin/combined && chmod +x /root/0g-da-client/disperser/bin/server
+
+sed -ie "s|https://[^ ]*|http://https://evm-rpc.0g.testnet.node75.org/|" /etc/systemd/system/daclient.service
+
+systemctl daemon-reload && systemctl restart daclient && journalctl -fu daclient
