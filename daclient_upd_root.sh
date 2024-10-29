0f70e058926f8db861d2c3022726d25964e06cae
KrakeNocto <octo2kraken@gmail.com>
2024-10-29 11:56:03 +0300

    Update daclient_upd_root.sh

--- daclient_upd_root.sh
+++ daclient_upd_root.sh
@@ -7,6 +7,12 @@ chmod +x $HOME/0g-da-client/disperser/bin/batcher && chmod +x $HOME/0g-da-client
 
 sed -ie "s|http://[^ ]*|https://evm-rpc.0g.testnet.node75.org/|" /etc/systemd/system/daclient.service
 
+sed -ie 's|indexer = "kv"|indexer = "null"|'  /root/.0gchain/config/config.toml
+
+systemctl restart ogd
+
+rm -rf /root/.0gchain/data/tx_index.db
+
 rm daclient_upd.sh
 
 systemctl daemon-reload && systemctl restart daclient && journalctl -fu daclient
