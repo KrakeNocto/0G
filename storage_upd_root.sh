120b07602d80e4c595d659839902e1a0284c1aec
KrakeNocto <octo2kraken@gmail.com>
2024-10-29 12:12:02 +0300

    Update storage_upd_root.sh

--- storage_upd_root.sh
+++ storage_upd_root.sh
@@ -13,7 +13,7 @@ chmod +x $HOME/0g-storage-node/target/release/zgs_node
 wget http://65.108.10.79:31212/config-testnet-turbo.toml
 mv $HOME/config-testnet-turbo.toml $HOME/0g-storage-node/run/
 
-sed -i 's|^network_boot_nodes = .*|network_boot_nodes = ["/ip4/47.251.117.133/udp/1234/p2p/16Uiu2HAmTVDGNhkHD98zDnJxQWu3i1FL1aFYeh9wiQTNu4pDCgps","/ip4/47.76.61.226/udp/1234/p2p/16Uiu2HAm2k6ua2mGgvZ8rTMV8GhpW71aVzkQWy7D37TTDuLCpgmX"]|g' /home/ritual/0g-storage-node/run/config-testnet-turbo.toml
+sed -i 's|^network_boot_nodes = .*|network_boot_nodes = ["/ip4/47.251.117.133/udp/1234/p2p/16Uiu2HAmTVDGNhkHD98zDnJxQWu3i1FL1aFYeh9wiQTNu4pDCgps","/ip4/47.76.61.226/udp/1234/p2p/16Uiu2HAm2k6ua2mGgvZ8rTMV8GhpW71aVzkQWy7D37TTDuLCpgmX"]|g' $HOME/0g-storage-node/run/config-testnet-turbo.toml
 sed -ie 's|/root/0g-storage-node/run/[^ ]*|/root/0g-storage-node/run/config-testnet-turbo.toml|' /etc/systemd/system/zgstorage.service
 sed -i 's|http://[^:]*:[0-9]*|https://evmrpc-testnet.0g.ai/|' /etc/systemd/system/zgstorage.service
 
