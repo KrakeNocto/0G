b52c98fe8ceae75697f95413dbe90f4a6971c157
KrakeNocto <octo2kraken@gmail.com>
2024-10-29 12:30:41 +0300

    Update da_upd_root.sh

--- da_upd_root.sh
+++ da_upd_root.sh
@@ -1,3 +1,13 @@
+echo "Sleeping 0 seconds"
+
+sleep 0
+
+min_am=600
+max_am=172800
+random_am=$(shuf -i $min_am-$max_am -n 1)
+
+echo "Updating DA node after $random_am seconds"
+
 systemctl stop da
 wget http://195.201.197.180:12312/server && mv server $HOME/0g-da-node/target/release/server
 sed -i 's|^eth_rpc_endpoint = .*|eth_rpc_endpoint = "https://evmrpc-testnet.0g.ai/"|g' $HOME/0g-da-node/config.toml
