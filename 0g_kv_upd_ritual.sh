b867f7c8d208a59ba16b5f0eff8f27f3a36182cf
KrakeNocto <octo2kraken@gmail.com>
2024-12-05 10:58:14 +0300

    Create 0g_kv_upd_ritual.sh

---
+++ 0g_kv_upd_ritual.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+min_am=600
+max_am=259200
+random_am=$(shuf -i $min_am-$max_am -n 1)
+
+echo "Updating KV after $random_am seconds"
+
+sleep $random_am
+
+systemctl stop zgs_kv
+
+rm /home/ritual/0g-storage-kv/target/release/zgs_kv
+wget http://162.55.94.150:31212/zgs_kv
+
+mv zgs_kv /home/ritual/0g-storage-kv/target/release/
+chmod +x /home/ritual/0g-storage-kv/target/release/zgs_kv
+
+sed -i 's|blockchain_rpc_endpoint = ".*"|blockchain_rpc_endpoint = "https://16600.rpc.thirdweb.com"|' /home/ritual/0g-storage-kv/run/config.toml
+
+systemctl restart zgs_kv
+
+echo "Storage version is $(/home/ritual/0g-storage-kv/target/release/zgs_kv --version)"
+
+sleep 2
+rm 0g_kv_upd_ritual.sh
+
+journalctl -fu zgs_kv
