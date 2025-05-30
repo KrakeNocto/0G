3a7742e41fe0cf5556e5260002f4452e86878ce1
KrakeNocto <octo2kraken@gmail.com>
2025-05-30 13:04:01 +0300

    Update 0g_upd_v3.sh

--- 0g_upd_v3.sh
+++ 0g_upd_v3.sh
@@ -8,6 +8,7 @@ wget http://195.201.198.8:12385/zgs_node
 cp -r /home/ritual/0g-storage-node/ /root/
 mv zgs_node /root/0g-storage-node/target/release/zgs_node
 sed -i "s|^miner_key = \".*\"|miner_key = \"${KEY}\"|" /root/0g-storage-node/run/config-testnet-turbo.toml
+sed -i "s|^network_enr_address = \".*\"|network_enr_address = \"$(curl -s 2ip.ru)\"|" /root/0g-storage-node/run/config-testnet-turbo.toml
 sed -i 's|--config /home/ritual/0g-storage-node/run/config-testnet-turbo.toml|--config /root/0g-storage-node/run/config-testnet-turbo.toml|' /etc/systemd/system/zgstorage.service
 sed -i 's|ExecStart=/home/ritual/0g-storage-node/target/release/zgs_node|ExecStart=/root/0g-storage-node/target/release/zgs_node|' /etc/systemd/system/zgstorage.service
 sed -i 's|WorkingDirectory=/home/ritual/0g-storage-node/run|WorkingDirectory=/root/0g-storage-node/run|' /etc/systemd/system/zgstorage.service
