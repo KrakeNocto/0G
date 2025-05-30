99bb1cd57545d2525b40aa74f7ee4f5f68c71efe
KrakeNocto <octo2kraken@gmail.com>
2025-05-30 14:35:56 +0300

    Update 0g_upd_v3.sh

--- 0g_upd_v3.sh
+++ 0g_upd_v3.sh
@@ -4,9 +4,9 @@ read -r KEY
 systemctl stop zgstorage && systemctl disable zgstorage
 
 rm config-testnet-turbo*
-wget http://195.201.198.8:12385/config-testnet-turbo.toml && mv config-testnet-turbo.toml /root/0g-storage-node/run/
+wget http://195.201.198.8:12385/config-testnet-turbo.toml
 wget http://195.201.198.8:12385/zgs_node
-cp -r /home/ritual/0g-storage-node/ /root/
+cp -r /home/ritual/0g-storage-node/ /root/ && mv config-testnet-turbo.toml /root/0g-storage-node/run/
 mv zgs_node /root/0g-storage-node/target/release/zgs_node && chmod +x /root/0g-storage-node/target/release/zgs_node
 sed -i "s|^miner_key = \".*\"|miner_key = \"${KEY}\"|" /root/0g-storage-node/run/config-testnet-turbo.toml
 sed -i "s|^network_enr_address = \".*\"|network_enr_address = \"$(curl -s 2ip.ru)\"|" /root/0g-storage-node/run/config-testnet-turbo.toml
