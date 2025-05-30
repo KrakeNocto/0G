1c897a1f4b00deb1c2dd3d5a263f7e96174f06bc
KrakeNocto <octo2kraken@gmail.com>
2025-05-30 14:34:14 +0300

    Update 0g_upd_v3.sh

--- 0g_upd_v3.sh
+++ 0g_upd_v3.sh
@@ -3,7 +3,7 @@ read -r KEY
 
 systemctl stop zgstorage && systemctl disable zgstorage
 
-rm config-testnet-turbo.toml
+rm config-testnet-turbo*
 wget http://195.201.198.8:12385/config-testnet-turbo.toml && mv config-testnet-turbo.toml /root/0g-storage-node/run/
 wget http://195.201.198.8:12385/zgs_node
 cp -r /home/ritual/0g-storage-node/ /root/
