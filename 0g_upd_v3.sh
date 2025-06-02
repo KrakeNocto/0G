daca76d4b3a4006bba769442aeeef5d9f8f42d36
KrakeNocto <octo2kraken@gmail.com>
2025-06-02 09:46:14 +0300

    Update 0g_upd_v3.sh

--- 0g_upd_v3.sh
+++ 0g_upd_v3.sh
@@ -14,4 +14,8 @@ sed -i 's|--config /home/ritual/0g-storage-node/run/config-testnet-turbo.toml|--
 sed -i 's|ExecStart=/home/ritual/0g-storage-node/target/release/zgs_node|ExecStart=/root/0g-storage-node/target/release/zgs_node|' /etc/systemd/system/zgstorage.service
 sed -i 's|WorkingDirectory=/home/ritual/0g-storage-node/run|WorkingDirectory=/root/0g-storage-node/run|' /etc/systemd/system/zgstorage.service
 
-systemctl daemon-reload && systemctl enable zgstorage && systemctl start zgstorage && tail -f /root/0g-storage-node/run/log/zgs.log.$(TZ=UTC date +%Y-%m-%d)
+systemctl daemon-reload && systemctl enable zgstorage && systemctl start zgstorage
+
+sleep 15
+
+tail -f /root/0g-storage-node/run/log/zgs.log.$(TZ=UTC date +%Y-%m-%d)
