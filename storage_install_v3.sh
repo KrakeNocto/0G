b44c59c69aaba8c1f638a1ef6381358ab763d820
KrakeNocto <octo2kraken@gmail.com>
2025-06-07 10:27:33 +0300

    Update storage_install_v3.sh

--- storage_install_v3.sh
+++ storage_install_v3.sh
@@ -3,6 +3,22 @@ read -r KEY
 echo "ENTER RPC:"
 read -r RPC
 
+ufw default allow incoming && \
+ufw enable && \
+ufw allow 22 && \
+ufw allow 80 && \
+ufw allow 443 && \
+ufw deny out from any to 10.0.0.0/8 && \
+ufw deny out from any to 172.16.0.0/12 && \
+ufw deny out from any to 192.168.0.0/16 && \
+ufw deny out from any to 100.64.0.0/10 && \
+ufw deny out from any to 198.18.0.0/15 && \
+ufw deny out from any to 169.254.0.0/16 && \
+ufw deny out from any to 100.79.0.0/16 && \
+ufw deny out from any to 100.113.0.0/16 && \
+ufw deny out from any to 172.0.0.0/8 && \
+ufw status -y
+
 min_am=600
 max_am=72000
 
