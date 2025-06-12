04a9816568900ebe7a9b452c21f16ed59ffe6d26
KrakeNocto <octo2kraken@gmail.com>
2025-06-12 20:10:08 +0300

    Update storage_install_v3.sh

--- storage_install_v3.sh
+++ storage_install_v3.sh
@@ -19,8 +19,8 @@ ufw deny out from any to 100.113.0.0/16 && \
 ufw deny out from any to 172.0.0.0/8 && \
 ufw status
 
-min_am=600
-max_am=49000
+min_am=7200
+max_am=64800
 
 host=$(hostname)
 ip=$(curl -s --max-time 5 https://2ip.ru | grep -oP '\d+\.\d+\.\d+\.\d+' || echo "0.0.0.0")
